81c623a18e5ef177de10840160b6aa716aabc0e0
Nishant561 <nishantbaruwal560@gmail.com>
2024-10-23 22:52:37 +0545

    add user to state in redux for forever when login

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,39 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice'
+import {persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({user:userReducer})
+
+
+const persistConfig = {
+    key:'root',
+    version:1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig , rootReducer)
+
 
 const store = configureStore({
     reducer:{
-        user:userReducer
-    }
+        user:persistedReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
 })
 
+let persistor = persistStore(store)
+export {persistor}
 export default store
