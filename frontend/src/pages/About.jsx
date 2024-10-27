e5c7cdca39ed8d8865f462084318d97ce0ff81ec
Nishant561 <nishantbaruwal560@gmail.com>
2024-10-27 22:24:24 +0545

    add about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import '../App.css'
 function About() {
   return (
-    <div className='universal'>
-      i am about
+    <div className='universal flex items-center justify-center'>
+      <div className='w-[800px]   items-center'>
+
+     <h1 className="font-semibold text-center text-white text-4xl"> About Me</h1>
+     <p className="text-justify pt-3 text-xl leading-7 ">
+     Hi! I'm Nishant Baruwal, a passionate full-stack engineer with experience in building modern web applications. I specialize in the MERN stack (MongoDB, Express, React, and Node.js) and have integrated Firebase for secure authentication and cloud functionalities.
+
+This project is a demonstration of my skills in creating an authenticated page app that provides seamless user experiences with efficient backend communication. I enjoy working on both the frontend and backend, delivering clean, functional code and scalable solutions.
+
+Feel free to explore the project, and I hope you enjoy it as much as I enjoyed building it!
+        </p>
+      
+      </div>
+
     </div>
   )
 }
