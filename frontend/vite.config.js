eeddfd0e5ce1a0a029ad42a3f90946a67c9b32ca
Nishant561 <nishantbaruwal560@gmail.com>
2024-10-28 15:55:28 +0545

    add backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:8000',
+        target:'https://login-functionality-m40j.onrender.com',
         changeOrigin:true
 
       }
